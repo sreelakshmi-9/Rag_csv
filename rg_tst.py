88bde0c542e09d88d621d8142ef498460aa6bc23
sreelakshmi-9 <126379260+sreelakshmi-9@users.noreply.github.com>
2025-07-16 01:39:04 +0530

    Update rg_tst.py

--- rg_tst.py
+++ rg_tst.py
@@ -74,7 +74,7 @@ import streamlit as st
 import pandas as pd
 import os
 import streamlit as st
-st.set_page_config(page_title="CSV Q&A Chatbot", layout="wide")
+#st.set_page_config(page_title="CSV Q&A Chatbot", layout="wide")
 
 import pandas as pd
 import os
